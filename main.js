604569daa74a18e2cf1142fd756a58c12efe571c
Nithish Nuthalapati <nuthalapatinithish1@gmail.com>
2025-07-25 17:57:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll("section, header");
+  const navLinks = document.querySelectorAll(".sidebar-icons a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
